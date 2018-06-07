4fad23f175210d4c233e0962d8cdfa6df930754b
zdy <zdy07015394@gmail.com>
2018-06-07 20:44:23 +0800

    pathchange4

--- js/hotSearch.js
+++ js/hotSearch.js
@@ -9,7 +9,7 @@ export class HotSearch {
     //this.$el.addEventListener('click', this.startSearch.bind(this))
   }
   getData(){
-    fetch('qq-music/json/hotSearch.json')
+    fetch('.././json/hotSearch.json')
       .then(res => res.json())
       .then(json => this.render(json))
   }
